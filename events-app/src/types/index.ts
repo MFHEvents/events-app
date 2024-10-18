bdd9a1cc7fa95a9201dceb7d7a29307ed132bb35
Ife-deb <ioyetoran@gmail.com>
2024-10-18 14:01:46 -0400

    create type definition for Event

--- events-app/src/types/index.ts
+++ events-app/src/types/index.ts
@@ -1,3 +1,5 @@
+import { RecurrencePattern } from '@/constants/constants';
+
 export interface ChurchEvent {
     id: number;
     title: string;
@@ -6,4 +8,16 @@ export interface ChurchEvent {
     location: string;
     summary: string;
     imageUrl: string;
+  }
+
+  export interface Event {
+    title: string;
+    date: Date;
+    location: string;
+    fee?: number;
+    summary: string;
+    description: string;
+    isRecurring?: boolean;
+    recurrencePattern?: RecurrencePattern;
+    image?: File
   }
